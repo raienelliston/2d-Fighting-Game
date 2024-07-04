475cd2fa2936f42e67ad6c718cc99f90da71e57b
Raien Elliston <118232707+only1Second@users.noreply.github.com>
2024-07-04 04:04:08 -0700

    tried to implement github cloning, failed

--- Game/scenes/LinkTextEdit.gd
+++
@@ -1,8 +0,0 @@
-extends TextEdit
-
-func _ready():
-	connect("text_changed", Callable(self, "_on_text_changed"))
-	
-func _on_text_changed():
-	var current_text = text
-	print("Text changed: ", current_text)
